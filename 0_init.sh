2a633edd3193e1d0b6e35bbabddcdbea7f06db4a
behappyman <a18091986>
2024-11-24 16:13:01 +0300

    init

--- 0_init.sh
+++ 0_init.sh
@@ -1,3 +1,5 @@
 cp 1_install_docker_grafana_prometheus.sh ..
 cp 2_reinstall_docker.sh ..
 cp 3_docker_clear.sh ..
+cd ..
+chmod +x 1_install_docker_grafana_prometheus.sh 2_reinstall_docker.sh 3_docker_clear.sh
